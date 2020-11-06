9fe427952a913a20cbe3d7bed4fe9c2b633058f1
Surajv <vsuraj2405@gmail.com>
2020-11-06 23:27:09 +0530

    Untrack files in .gitignore
    added code for bluetooth work

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+ListView listView;
+TextView statusTextView;
+Button searchButton;
+BluetoothAdapter bluetoothAdapter; 
+
+    public void searchClicked(View view){
+
+        statusTextView.setText("Searching devices...");
+//        not to lauch the process everytime ...
+        searchButton.setEnabled(false);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        statusTextView = findViewById(R.id.statusTextView);
+        searchButton = findViewById(R.id.searchButton);
+
+
     }
 }
